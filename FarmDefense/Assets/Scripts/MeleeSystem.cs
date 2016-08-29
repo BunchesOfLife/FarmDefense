cca4489b444ea2d2d59271261a4b17a59df3c690
Brian Picciano <BJPicciano@gmail.com>
2016-08-29 16:10:43 -0400

    Added an attack/animation for the pitchfork.

---
+++ FarmDefense/Assets/Scripts/MeleeSystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeSystem : MonoBehaviour {
+
+    public Camera MainCamera;
+    public float MaxRange;
+    public float Range;
+    public int Damage;
+	
+	// Update is called once per frame
+	void Update() {
+
+        if (Input.GetButtonDown("Fire1")) {
+            GetComponent<Animation>().Play();
+        }
+	}
+
+    void Attack() {
+        RaycastHit hitRay;
+        Transform CameraTransform = MainCamera.transform;
+        if (Physics.Raycast(CameraTransform.position, CameraTransform.TransformDirection(Vector3.forward), out hitRay)) {
+            Range = hitRay.distance;
+            if (hitRay.distance < MaxRange) {
+                hitRay.transform.SendMessage("TakeDamage", Damage, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+    }
+}
